4893018871984416b402358c809146013fa67650
Nishant Verma <57328381+nverma4700@users.noreply.github.com>
2024-06-12 21:06:08 +0530

    Adding script to find runner-up score

---
+++ scripts/runner_up.py
@@ -0,0 +1,25 @@
+#Find the Runner-Up Score. 
+#You are given n scores. Store them in a list and find the score of the runner-up.
+
+# Sample input = 2 3 6 6 5
+# Sampls input = 6 6 6 6 6 6 6 6 6 5
+
+
+if __name__ == '__main__':
+    score = input('Enter the score: ')
+    arr = list(map(int, input().split()))
+    sorted_arr = sorted(arr, reverse=True)
+    if len(arr) > 2:
+        for i in range(len(arr) -1):
+            if sorted_arr[i] != sorted_arr[i+1]:
+                runner_up = sorted_arr[i+1]
+                break
+        if runner_up:
+            print(runner_up)
+        else: 
+            print('Runner up not found')
+    else:
+        print('Length of score list is not sufficient to find the runnerup')
+
+
+        
